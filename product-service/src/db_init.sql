d2db88bf723fdda0cc36753bed6713b49f2b733e
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2020-11-08 23:44:18 +0300

    task-4: fix sql script

--- product-service/src/db_init.sql
+++ product-service/src/db_init.sql
@@ -4,16 +4,15 @@ CREATE TABLE product (
 	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	title text NOT NULL,
 	description text,
-	price integer,
+	price text,
 	photo text
 );
-
 
 CREATE TABLE stock (
 	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	product_id uuid,
 	count integer,
-	FOREIGN KEY ("product_id") REFERENCES "product" ("id")
+	FOREIGN KEY ("product_id") REFERENCES "product" ("id") ON DELETE CASCADE
 );
 
 INSERT INTO
